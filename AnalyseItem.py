84057d548326829db9dbd246b2b6dc2e3e10d8a8
boyce <838304814@qq.com>
2017-11-04 10:17:38 +0800

    增加一些model

--- AnalyseItem.py
+++ AnalyseItem.py
@@ -1,5 +1,12 @@
 
-class RelatingItem:
+class RelatingGroup:
 
     def __init__(self):
-        print ("init")
+        self.items = None
+        self.groupName = None
+
+
+class RelatingItem:
+    def __init__(self):
+        self.name = None
+        self.importItems = None
